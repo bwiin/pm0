de707f2c30db5517f887fc87a321430d2e168845
bwiin <wiinbrian@gmail.com>
2016-09-22 19:47:18 -0400

    Fixed formatting v2
    My take on the whitespace and

--- vm.c
+++ vm.c
@@ -65,35 +65,21 @@ void main(int argc, char *argv[]){
     stack[1]=0;
 	stack[2]=0;
 
- 	// print current contents of the instruction array for testing purposes (It prints to stdout
- 	//printf("\n The following is the current instruction set:\n\n");
- 	//	for(i =0; i<numofinstructions; i++){
-    //    printf(" \n\t Line %d contents: %3d  %2d  %3d ", i+1, ir[i].op,  ir[i].l, ir[i].m);
-	//}
-	//printf(" \n\n\n\n\n"); // print some space for the instructions
-	// end of test print
-
-	//outputfile = fopen( OUT, "w"); //open output file to write out to file.
-
-	// print out the first portion
+	// print out the first portion(input file)
     printf("PL/0 code:\n\n");
-	for(i = 0; i < numofinstructions; i++){
-
+	for(i = 0; i < numofinstructions; i++)
+    {
 		strcpy(instructionWord,opname(ir[i]));
 
 		//if we use STO or CAL, the level must be printed out differntly.
-		if (ir[i].op == 2 || ir[i].op == 9) {
-			printf("%3d%5s\n",i, instructionWord, ir[i].m);
-		} else if (ir[i].op == 4 || ir[i].op == 5 ){
+		if (ir[i].op == 2 || ir[i].op == 9)
+            printf("%3d%5s\n",i, instructionWord, ir[i].m);
+		else if (ir[i].op == 4 || ir[i].op == 5 )
 			printf("%3d%5s%5d%5d\n",i, instructionWord, ir[i].l,ir[i].m);
-		} else {
+		else
 			printf("%3d%5s%10d\n",i, instructionWord, ir[i].m);
-		}
 	}
 	printf("\n" );
-    //fprintf(outputfile,"\n\n");
-	// end of printing the first portion
-
 
 /*
  *
@@ -111,99 +97,92 @@ void main(int argc, char *argv[]){
  	printf("Execution:\n");
  	printf("%24s%5s%5s%8s\n", "pc","bp","sp","stack");
 	printf("%24d%5d%5d\n", pc, bp, sp);
-	while(bp > 0 && halt != 1){
-		if(pc < numofinstructions){
-
-			fetch();
-			if (cur_instr.op == 5 || cur_instr.op == 4) {
+	while(bp > 0 && halt != 1)
+	{
+        if(pc < numofinstructions)
+        {
+            fetch();
+            if (cur_instr.op == 5 || cur_instr.op == 4)
 				printf("%3d%5s%5d%5d", pc-1, opname(ir[pc-1]), ir[pc-1].l, ir[pc-1].m);
-			} else if (cur_instr.op == 2 || cur_instr.op == 9){
+            else if (cur_instr.op == 2 || cur_instr.op == 9)
 				printf("%3d%5s%10s", pc-1, opname(ir[pc-1]), " ");
-			} else {
+			else
 				printf("%3d%5s%10d", pc-1, opname(ir[pc-1]), ir[pc-1].m);
-			}
 
 			choose();
 			printf("%6d%5d%5d   ", pc, bp, sp);
 			outputstack(outputfile);
-		} else {
+		}
+		else
+		{
 			printf("\n\ncontrol goes here\n\n");
 			exit(1);
 		}
 
-	printf("\n");
-}
-
-
-
-
+        printf("\n");
+    }
 }
 
 //written by Ryan Hoeck
 //this function was taken and modified from one of my previous programs
-int commandParser(char *filename){
+int commandParser(char *filename)
+{
 	FILE *fp;
     char *line;
     char *temp;
     char *token;
 	int n;
 
-    if (filename == NULL) {
+    if (filename == NULL)
+    {
 		perror("You did not input a filename\n");
         return -1;
     }
 
     fp = fopen(filename,"r");
-    if (fp == NULL) {
+
+    if (fp == NULL)
+    {
 		perror("You did not input a valid filename\n");
         return -1;
     }
 
-
     line = malloc(sizeof(char) * MAX_CODE_LENGTH + 1);
     line = fgets(filename, MAX_CODE_LENGTH + 1, fp);
 
-
-	if ( line == NULL) {
+	if ( line == NULL)
 		return -1;
-	}
 
-    while (line != NULL) {
+    while (line != NULL)
+    {
         token = strtok(line," ");
-		if (token != NULL) {
+		if (token != NULL)
 			ir[numofinstructions].op = atoi(token);
-		}
 
 		token = strtok(NULL," ");
-		if (token != NULL) {
+		if (token != NULL)
 			ir[numofinstructions].l = atoi(token);
-		}
 
 		token = strtok(NULL," ");
-		if (token != NULL) {
+		if (token != NULL)
 			ir[numofinstructions].m = atoi(token);
-		}
-
 
 		line = fgets(filename, MAX_CODE_LENGTH + 1, fp);
 		numofinstructions++;
     }
-
 	free(line);
     fclose(fp);
 
 	return 0;
 }
-
 /*
  * fetch function
  */
-void fetch(){
-        cur_instr = ir[pc];
-        pc++;
+void fetch()
+{
+    cur_instr = ir[pc];
+    pc++;
 }
-
-
 /*
  *
  *
@@ -211,18 +190,19 @@ void fetch(){
  *
  *  Base function provided in the instructions, modified for global variables.
  */
-int base(int base, int L){
+
+int base(int base, int L)
+{
+
     int b1; //find base L levels down
     b1 = base;
-    while (L > 0){
+    while (L > 0)
+    {
         b1 = stack[b1 + 1];
         L--;
     }
     return b1;
 }
-
-
-
 /*
  *
  *
@@ -230,75 +210,60 @@ int base(int base, int L){
  *
  *  initiates the appropriate function correlating to the appropriate op code.
  */
-void choose(){
+void choose()
+{
     switch (cur_instr.op){
 
         case 1:
-			//printf("a\n");
             lit();
             break;
 
         case 2:
-		//	printf("b\n");
             opr();
             break;
 
         case 3:
-			//printf("c\n");
             lod();
             break;
 
         case 4:
-		//	printf("d\n");
             sto();
             break;
 
         case 5:
-			//printf("e\n");
             cal();
             break;
 
         case 6:
-			//printf("f\n");
             inc();
             break;
 
         case 7:
-			//printf("g\n");
             jmp();
             break;
 
         case 8:
-		//	printf("h\n");
             jpc();
             break;
 
         case 9:
-		//	printf("i\n");
             sio();
             break;
 
         case 10:
-			//printf("j\n");
             sio();
             break;
 
         case 11:
-			//printf("k\n");
             sio();
             break;
 
         default:
-			//printf("l\n");
 			halt = 1;
             exit(0);
             break;
     }
-
-
 }
-
-
 /*
  *
  *
@@ -306,11 +271,11 @@ void choose(){
  *
  *  performs the LIT operation as outlined in the homework
  */
-void lit(){
+void lit()
+{
     sp++;
     stack[sp] = cur_instr.m;
 }
-
 /*
  *
  *
@@ -318,91 +283,91 @@ void lit(){
  *
  *  performs the OPR operation as outlined in the homework
  */
-void opr(){
-
-    if(cur_instr.m == 0){
+void opr()
+{
+    if(cur_instr.m == 0)
+    {
         sp = bp - 1;
         bp = stack[sp + 3];
         pc = stack[sp + 4];
-
     }
-
-
-    if(cur_instr.m == 1){
+    if(cur_instr.m == 1)
         stack[sp] = -stack[sp];
-    }
 
-    if(cur_instr.m == 2){
+    if(cur_instr.m == 2)
+    {
         stack[sp] = stack[sp] + stack[sp + 1];
         sp = sp - 1;
     }
 
-    if(cur_instr.m == 3){
+    if(cur_instr.m == 3)
+    {
         stack[sp] = stack[sp] - stack[sp + 1];
         sp = sp - 1;
     }
 
 
-    if(cur_instr.m == 4){
+    if(cur_instr.m == 4)
+    {
         stack[sp] = stack[sp] * stack[sp + 1];
         sp = sp - 1;
     }
 
-    if(cur_instr.m == 5){
+    if(cur_instr.m == 5)
+    {
         stack[sp] = stack[sp] / stack[sp + 1];
         sp = sp - 1;
     }
 
-    if(cur_instr.m == 6){
+    if(cur_instr.m == 6)
         stack[sp] = stack[sp] % 2;
 
-    }
 
-    if(cur_instr.m == 7){
-
+    if(cur_instr.m == 7)
+    {
         stack[sp] = stack[sp] % stack[sp + 1];
         sp = sp - 1;
-
     }
 
-    if(cur_instr.m == 8){
+    if(cur_instr.m == 8)
+    {
         stack[sp] =  ((stack[sp] == stack[sp + 1]) ? 1 : 0);
         sp = sp - 1;
     }
 
-    if(cur_instr.m == 9){
+    if(cur_instr.m == 9)
+    {
         stack[sp] =  ((stack[sp] != stack[sp + 1]) ? 1 : 0);
         sp = sp - 1;
     }
 
-    if(cur_instr.m == 10){
+    if(cur_instr.m == 10)
+    {
         stack[sp] =  ((stack[sp + 1] - stack[sp])>0 ? 1 : 0);
         sp = sp - 1;
-
     }
 
-    if(cur_instr.m == 11){
+    if(cur_instr.m == 11)
+    {
         stack[sp] =  ((stack[sp + 1] - stack[sp])>=0 ? 1 : 0);
         sp = sp - 1;
     }
 
-    if(cur_instr.m == 12){
+    if(cur_instr.m == 12)
+    {
         stack[sp] =  (stack[sp] - (stack[sp + 1])>0 ? 1 : 0);
         sp = sp - 1;
     }
 
-    if(cur_instr.m == 13){
+    if(cur_instr.m == 13)
+    {
         stack[sp] =  (stack[sp] - (stack[sp + 1])>=0 ? 1 : 0);
         sp = sp - 1;
     }
 
     if(cur_instr.m != 0)
         pc++;
-
 }
-
-
-
 /*
  *
  *
@@ -410,13 +375,12 @@ void opr(){
  *
  *  performs the LOD operation as outlined in the homework
  */
-void lod(){
+void lod()
+{
     sp = sp + 1;
     stack[sp] = stack[ base(bp, cur_instr.l) + cur_instr.m];
     pc++;
 }
-
-
 /*
  *
  *
@@ -424,13 +388,11 @@ void lod(){
  *
  *  performs the STO operation as outlined in the homework
  */
-void sto(){
+void sto()
+{
     stack[ base(bp, cur_instr.l) + cur_instr.m] = stack[sp];
     sp--;
 }
-
-
-
 /*
  *
  *
@@ -438,8 +400,8 @@ void sto(){
  *
  *  performs the CAL operation as outlined in the homework
  */
-void cal(){
-
+void cal()
+{
     ncalls++;
     stack[sp + 1] = 0; // space to return value (FV)
     stack[sp + 2] = base(bp, cur_instr.l); // static link (SL)
@@ -447,12 +409,7 @@ void cal(){
     stack[sp + 4] = pc; // return address (RA)
     bp = sp + 1;
     pc = cur_instr.m;
-
-
 }
-
-
-
 /*
  *
  *
@@ -461,14 +418,10 @@ void cal(){
  *
  *  performs the INC operation as outlined in the homework
  */
-void inc(){
-
+void inc()
+{
     sp+= cur_instr.m;
-
 }
-
-
-
 /*
  *
  *
@@ -476,13 +429,10 @@ void inc(){
  *
  *  performs the JMP operation as outlined in the homework
  */
-void jmp(){
-
+void jmp()
+{
     pc = cur_instr.m;
-
 }
-
-
 /*
  *
  *
@@ -497,9 +447,6 @@ void jpc(){
     sp--;
 
 }
-
-
-
 /*
  *
  *
@@ -507,24 +454,25 @@ void jpc(){
  *
  *  performs the SIO operation as outlined in the homework
  */
-void sio(){
+void sio()
+{
 
-    if(cur_instr.m == 0){
+    if(cur_instr.m == 0)
+    {
         printf("\n\n\t Contents: %d", stack[sp]);
         sp = sp - 1;
     }
 
-    if(cur_instr.m == 1){
+    if(cur_instr.m == 1)
+    {
         scanf("\n\n\t Please enter value: %d", &stack[sp+1]);
-	sp++;
+        sp++;
     }
 
-    if(cur_instr.m == 2){
+    if(cur_instr.m == 2)
         halt = 1;
-    }
 
 }// end sio
-
 
 
 /*
@@ -534,15 +482,17 @@ void sio(){
  *
  *  returns a string of the operation name based on it's integer representation.
  */
-char *opname(instruction op){
-
-    switch (op.op){
+char *opname(instruction op)
+{
+    switch (op.op)
+    {
         case 1:
             return "LIT";
             break;
 
         case 2:
-            switch (op.l) {
+            switch (op.l)
+            {
             	case 0:
 					return "RET";
 					break;
@@ -638,35 +588,38 @@ char *opname(instruction op){
             break;
     }
 }
-
-
 /*
  *
  * Prints the current stack contents with proper formatting
  *
  *
  */
-void outputstack(){
-        int tempBp = bp;
-        int bpCount = 1;
-        int levels[MAX_LEXI_LEVELS];
-        int j = 1;
-        levels[0] = 1;
+void outputstack()
+{
+    int tempBp = bp;
+    int bpCount = 1;
+    int levels[MAX_LEXI_LEVELS];
+    int j = 1;
+    levels[0] = 1;
 
-        while (tempBp > 1){
+    while (tempBp > 1)
+    {
         levels[j++] = tempBp;
         tempBp = stack[tempBp + 2];
-        }
-    bpCount = j-1;
-    if(bpCount > MAX_LEXI_LEVELS){
-        printf("\nError: :: ");
     }
-        for(i = j; j <= sp; j++) {
-            if (j == levels[bpCount] && j != 1) {
-                printf("| ");
-                bpCount--;
-            }
-            printf("%d ", stack[j]);
+
+    bpCount = j-1;
+
+    if(bpCount > MAX_LEXI_LEVELS)
+        printf("\nError: :: ");
+
+    for(i = j; j <= sp; j++)
+    {
+        if (j == levels[bpCount] && j != 1)
+        {
+            printf("| ");
+            bpCount--;
         }
-	//fprintf(fileTrace,"\n");
+        printf("%d ", stack[j]);
+    }
 }
